0cd769ca13c974339d44ffa48618780cfaf77164
jaimeCardenas176 <jaime.cardenas176gmail.com>
2018-06-16 13:35:24 +0200

    en teoria terminada pero no funciona...

--- skypetAPI/routes/pets.js
+++ skypetAPI/routes/pets.js
@@ -7,6 +7,6 @@ let router = express.Router();
 
 router.post('/add', petController.add_pet);
 router.get('/list', petController.list_pet);
-router.delete('/delete', petController.delete_pet);
-router.put('/edit', petController.edit_pet);
-router.put('/pet_details', petController.pet_details);
+router.delete('/delete/:id', petController.delete_pet);
+router.put('/edit/:id', petController.edit_pet);
+router.put('/pet_details/:id', petController.pet_details);
